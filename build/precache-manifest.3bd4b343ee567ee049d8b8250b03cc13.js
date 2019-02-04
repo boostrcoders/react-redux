89f091794e890f9052f593e63bdc9a3ded35125f
boostrcoders <41163272+boostrcoders@users.noreply.github.com>
2019-02-04 12:07:28 +0800

    deploy

---
+++ build/precache-manifest.3bd4b343ee567ee049d8b8250b03cc13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d916940f69d5348bd0e0aa1b181a1c5b",
+    "url": "/react-redux/static/media/mylogo2.d916940f.png"
+  },
+  {
+    "revision": "ab0783d5d9c0f0db6c3f",
+    "url": "/react-redux/static/js/runtime~main.ab0783d5.js"
+  },
+  {
+    "revision": "61f45e083f895d92bd85",
+    "url": "/react-redux/static/js/main.61f45e08.chunk.js"
+  },
+  {
+    "revision": "cd4a610248263d7ae365",
+    "url": "/react-redux/static/js/1.cd4a6102.chunk.js"
+  },
+  {
+    "revision": "61f45e083f895d92bd85",
+    "url": "/react-redux/static/css/main.00160bd4.chunk.css"
+  },
+  {
+    "revision": "86e7bf7445f9095044cec0097eb2be96",
+    "url": "/react-redux/index.html"
+  }
+];
